3980ed6d699585bc2ec56ae5093a17c0b7c6f604
Jheimys Bassani <jheimysbb@gmail.com>
2022-04-16 21:06:58 -0300

    criando as primeiras rotas

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
 
+const routes = require('./routes/routes')
+
 const app = express()
 
 //habilita para receberdados via post
 app.use(express.urlencoded({extended:true}))
+
+app.use('/api', routes)
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listenig is port ${port} `))
