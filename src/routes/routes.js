31353ad68850ee27150d8f91f0ba8d2bf3139118
Jheimys Bassani <jheimysbb@gmail.com>
2022-04-17 21:27:27 -0300

    Atualizando o metodo get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,6 @@
 const router = require('express').Router()
 const ProductsControllers = require('../controllers/products')
 
-router.get('/products',  ProductsControllers.get)
+router.get('/products/:id?',  ProductsControllers.get)
 
 module.exports = router
