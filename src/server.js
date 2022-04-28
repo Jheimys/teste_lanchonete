3504e652d1390c2a95fa708408c43df3e6d64f7f
Jheimys Bassani <jheimysbb@gmail.com>
2022-04-28 15:37:36 -0300

    Finalizando atualização de cadastro

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 
 const routes = require('./routes/routes')
@@ -13,6 +14,7 @@ const app = express()
 //app.use(express.urlencoded({extended:true}))
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api', routes)
 
